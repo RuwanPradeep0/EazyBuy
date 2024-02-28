0fc6832e97dfc3d4a1d3f5229dabfd53a9fb8a56
Ruwan Pradeep <ruwanpradeep9911@gmail.com>
2024-02-29 00:57:32 +0530

    errors are fixed

--- frontend/src/components/Layouts/Header/Header.jsx
+++ frontend/src/components/Layouts/Header/Header.jsx
@@ -4,12 +4,12 @@ import logo from '../../../assests/logo.png';
 
 const Header = () => {
   return (
-    <header className="bg-lime-800">
-        <div >
+    <header className="bg-primary-blue fixed top-0 py-2.5 w-full z-10">
+        <div className="w-full sm:w-9/12 px-1 sm:px-4 m-auto flex justify-between items-center relative">
            
-           <div >
+           <div className="flex items-center flex-1">
             
-           <Link to='/' >
+           <Link className="h-10 mr-1 sm:mr-4" to='/' >
                 <img draggable='false' src={logo} alt='logo'/>
            </Link>
 
